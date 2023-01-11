aafbf966097634accd6121a52ec450c332b5b95c
Paul Blischak <paul.blischak@bayer.com>
2023-01-11 15:52:29 -0500

    removing duplicated null-ls use statement

--- nvim/lua/pblischak/packer.lua
+++ nvim/lua/pblischak/packer.lua
@@ -3,14 +3,6 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use({
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-        require("null-ls").setup()
-    end,
-    requires = { "nvim-lua/plenary.nvim" },
-  })
-
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  requires = {
